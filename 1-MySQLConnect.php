4fa1ec7db8d912fd28f8dc8d0977f2b32854a4b5
jalilaziz <jaliilaziiz@gmail.com>
2024-02-28 15:30:53 +0000

    php briefs

---
+++ 1-MySQLConnect.php
@@ -0,0 +1,79 @@
+<?php
+//PDO
+
+$connectSuccess = $connectFailed = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (isset($_POST["submit"])){
+        $serverName = "localhost";
+        $userName = "root";
+        $password = "";
+        $dbName = "solidb";
+
+        try {
+            $conn = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $connectSuccess = "Connected successfully";
+            //echo '<p style="color:green">Connected successfully</p>';
+        } catch(PDOException) {
+            $connectFailed = "Connection failed";
+            //echo '<p style="color:red">Connection failed</p>';
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Connect to MySQL DATABASE</title>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 550px;
+            border-radius: 15px;
+            border: 1px solid;
+            box-shadow: 2px 2px 4px;
+            padding: 20px;
+        }
+
+        .submit {
+            font-size: 15px;
+            background-color: #B6D7A8;
+            border-radius: 12px;
+            border: none;
+            padding: 20px 24px;
+        }
+
+        #connsuccess {
+            color: green;
+        }
+
+        #connfailed {
+            color: green;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>PHP Connect to MySQL DATABASE</h1>
+        <form class="formsubmit" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <button class="submit" type="submit" name="submit">Connect to database</button>
+            <p id="connsuccess">
+                <?php echo $connectSuccess?>
+            </p>
+            <p id="connfailed">
+                <?php echo $connectFailed?>
+            </p>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ 2-CreateDB.php
@@ -0,0 +1,86 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Create a MySQL DATABASE</title>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 550px;
+            border-radius: 15px;
+            border: 1px solid;
+            box-shadow: 2px 2px 4px;
+            padding: 20px;
+        }
+
+        .submit {
+            font-size: 15px;
+            background-color: #B6D7A8;
+            border-radius: 12px;
+            border: none;
+            padding: 20px 24px;
+        }
+
+        #connsuccess {
+            color: green;
+            /* font-size: 20px; */
+        }
+
+        #connfailed {
+            color: green;
+            /* font-size: 20px; */
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>PHP Create a MySQL DATABASE</h1>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <button class="submit" type="submit" name="submit">Create database</button>
+            <p id="connsuccess">
+                <?php echo $creatSuccess?>
+            </p>
+            <p id="connfailed">
+                <?php echo $creatFailed?>
+            </p>
+            <p id="dbexist">
+                <?php echo $exist?>
+            </p>
+        </form>
+    </div>
+    
+<?php
+//PDO
+
+$creatSuccess = $creatFailed = $exist = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (isset($_POST["submit"])){
+        $serverName = "localhost";
+        $userName = "root";
+        $password = "";
+        $dbName = "solidatabase";
+
+        try {
+            $conn = new PDO("mysql:host=$serverName", $userName, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $crea = "CREATE DATABASE $dbName ";
+            $conn -> exec($crea);
+            $creatSuccess = "Database Created successfully with name $dbName";
+        } catch(PDOException) {
+            $createFailed = "Error creating database";
+        }
+    }
+}
+?>
+</body>
+
+</html>
