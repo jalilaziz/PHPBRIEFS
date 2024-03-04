6a20e01202c1bb9b7793bd92c0b1c6fcd5f8eec7
Abdeljalil Aziz <125446723+jalilaziz@users.noreply.github.com>
2024-03-04 16:30:30 +0100

    Update 1-MySQLConnect.php

--- 1-MySQLConnect.php
+++ 1-MySQLConnect.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             color: green;
         }
         #failed {
-            color: green;
+            color: red;
         }
     </style>
 </head>
